10a8972f3bb88ec8ba9ffd12b53faa940f7a2b6b
Saydum Halibekov <saydum.it5@gmail.com>
2022-04-09 18:05:21 +0300

    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.

--- missingNumber.js
+++ missingNumber.js
@@ -1,9 +1,10 @@
 const nums = [9,6,4,2,3,5,7,0,1];
 
 function missingNumber(nums) {
-    const lenght = nums.length;
+
     const sum = nums.map(i => x += i, x = 0).reverse()[0];
-    return lenght * (lenght + 1) / 2 - sum;
+    return nums.length * (nums.length + 1) / 2 - sum;
+
 };
 
 missingNumber(nums);
